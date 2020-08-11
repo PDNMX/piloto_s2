351517f8bc8095691f014916b20500f3c3be39e6
ecamargo-eng <ecamargo@txmglobal.com>
2020-08-11 11:56:42 -0500

    add validation token

--- controllers/Spic.js
+++ controllers/Spic.js
@@ -2,10 +2,40 @@
 
 var utils = require('../utils/writer.js');
 var Spic = require('../service/SpicService');
+var jwt = require('jsonwebtoken');
+require('dotenv').config({path: './utils/.env'});
 
-module.exports.get_dependencias = async function get_dependencias (req, res, next) {
-    let dependencias = await Spic.getDependencias();
-    utils.writeJson(res,dependencias);
+
+var validateToken = function(req){
+    var inToken = null;
+    var auth = req.headers['authorization'];
+    if (auth && auth.toLowerCase().indexOf('bearer') == 0) {
+        inToken = auth.slice('bearer '.length);
+    } else if (req.body && req.body.access_token) {
+        inToken = req.body.access_token;
+    } else if (req.query && req.query.access_token) {
+        inToken = req.query.access_token;
+    }
+    // invalid token - synchronous
+    try {
+        var decoded =  jwt.verify(inToken, process.env.SEED );
+        return {code: 200, message: decoded};
+    } catch(err) {
+        // err
+        let obj = {code: 401, message: err};
+        return obj;
+    }
+}
+
+async function get_dependencias (req, res, next) {
+     var code = validateToken(req);
+
+     if(code.code == 401){
+         res.status(401).json({code: '401', message: code.message});
+     }else if (code.code == 200 ){
+         let dependencias = await Spic.getDependencias();
+         utils.writeJson(res,dependencias);
+     }
 };
 
 module.exports.post_spic = function post_spic (req, res, next, body) {
@@ -17,3 +47,6 @@ module.exports.post_spic = function post_spic (req, res, next, body) {
       utils.writeJson(res, response);
     });
 };
+
+
+module.exports.get_dependencias = get_dependencias;
